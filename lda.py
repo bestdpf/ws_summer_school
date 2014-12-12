e47de6bf97956424036947a8a5f6d1eae65d4c7b
pengfeituan-f18 <pengfeituan@gmail.com>
2014-12-12 10:43:49 +0800

    get msg date id

--- lda.py
+++ lda.py
@@ -1,4 +1,5 @@
 from gensim import corpora, models, similarities
+import itertools
 
 def lda(doc, topicNum):
     stopList = set ('for a of the and to in'.split())
@@ -25,6 +26,45 @@ def fastReadFile(filePath):
     f_in.close()
     return doc
 
+def cmpDate(date0, date1):
+    if date0[0]< date1[0]:
+        return True
+    elif date0[0] == date1[0] and date0[1]< date1[1]:
+        return True
+    elif date0[0] == date1[0] and date0[1] == date1[1]:
+        return date0[2] < date1[2]
+    else:
+        return False
+
+# from 2013-12-29 to 2013-06-02
+def calcDateId(dateInfo):
+    mnCnt = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+    mnInc = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365] 
+    if dateInfo[0] == 2013:
+        return dateInfo[2]-29
+    else:
+        return 2+mnInc[dateInfo[1]-1] + dateInfo[2]
+
+def timeSplit():
+    minDate = [2100,12,31]
+    maxDate = [2000,1,1]
+    f_in = open('tweet_six_label.txt','r')
+    f_t = open('tweet_time.txt','w')
+    for line in f_in.readlines():
+        lst = line.replace('&','').replace('#','').replace('\\','').replace('@','').replace(',','').replace('.','').replace('!','').replace('!','').split()
+        item = ' '.join(lst[4::])
+        date, time = lst[2].replace('T',' ').split()
+        date_info = map(int,date.replace('-',' ').split())
+        dateId = calcDateId(date_info)
+        print dateId
+        if cmpDate(maxDate, date_info):
+            maxDate = date_info[:]
+        if cmpDate(date_info, minDate):
+            minDate = date_info[:]
+    f_in.close()
+    f_t.close()
+    print minDate,maxDate
+
 def test():
     f_in = open('tweet_six_label.txt','r')
     f_doc = open('tweet_content.txt','w')
@@ -35,7 +75,7 @@ def test():
         f_doc.write('\n')
     f_in.close()
     f_doc.close()
-    doc = fastReadFile('tweet_content.txt')
+    doc = seqReadFile('tweet_content.txt')
     print doc[0]
     ldaIns = lda(doc, 100)
     ldaIns.save('ret.lda')
